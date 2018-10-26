c651ddc5848b7390a58371d3bfad628ff42c74a7
Kyle Tomsic <kyle.a.tomsic@gmail.com>
2018-10-25 23:44:07 -0400

    Make the ConfigChanged and ConfigChanges fields public
    If clients wish to handle the ConfigChanged event, they will need to be
    able to examine it and its ConfigChanges member describing the changes.

--- src/structs/config.rs
+++ src/structs/config.rs
@@ -2,16 +2,16 @@ use ViewId;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ConfigChanged {
-    view_id: ViewId,
-    changes: ConfigChanges
+    pub view_id: ViewId,
+    pub changes: ConfigChanges
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ConfigChanges {
-    font_face: Option<String>,
-    font_size: Option<u64>,
-    line_ending: Option<String>,
-    plugin_search_path: Option<Vec<String>>,
-    tab_size: Option<u64>,
-    translate_tabs_to_spaces: Option<bool>,
+    pub font_face: Option<String>,
+    pub font_size: Option<u64>,
+    pub line_ending: Option<String>,
+    pub plugin_search_path: Option<Vec<String>>,
+    pub tab_size: Option<u64>,
+    pub translate_tabs_to_spaces: Option<bool>,
 }
